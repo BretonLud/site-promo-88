fbad8aab8bc12a7adb65ce1f036cdc2b6007cf45
BretonLud <godneo@hotmail.fr>
2021-11-23 15:56:32 +0100

    first commit

---
+++ wp-content/themes/nisarg-child/footer.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after
+ *
+ * @package Nisarg
+ */
+?>
+</div><!-- #content -->
+<footer id="colophon" class="site-footer" role="contentinfo">
+    <div class="site-info">
+        <p>6 avenue du Maréchal Juin, 70200 Lure</p>
+        <div class="site-footer__commits">
+            <p>Derniers commits :</p>
+            <div class="site-footer__commit">
+                <p>1</p>
+                <p>2</p>
+                <p>3</p>
+            </div>
+        </div>
+    </div><!-- .site-info -->
+</footer><!-- #colophon -->
+</div><!-- #page -->
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ wp-content/themes/nisarg-child/functions.php
@@ -0,0 +1,12 @@
+<?php
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+
+function theme_enqueue_styles(){
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+}
+
+
+function my_custom_scripts() {
+    wp_enqueue_script( 'nisarg', get_stylesheet_directory_uri() . '/script.js');
+}
+add_action( 'wp_enqueue_scripts', 'my_custom_scripts' );
