e44f6873895d61f17efb8b9498066d25710bbf55
BretonLud <godneo@hotmail.fr>
2021-11-26 16:00:45 +0100

    update CSS JS

--- wp-content/themes/nisarg-child/src/header.js
+++ wp-content/themes/nisarg-child/src/header.js
@@ -9,7 +9,6 @@ if (location === 'post-101'){
     homeLink.style.display = 'none'
     siteBranding.style.display = "flex"
     siteBranding.style.flexDirection = "column"
-    siteBranding.style.maxWidth = "400px"
     siteBranding.innerHTML = "<h1 class='site-title'>Les exposés</h1><p class='presentation__header'>Durant notre formation, nous avons fait différentes présentations que vous retrouverez ci-joint en téléchargement</p>"
     siteHeader.style.background = "url('../wp-content/themes/nisarg-child/css/images/bgCodage.jpeg')";
     siteHeader.style.backgroundSize = "cover";
@@ -18,14 +17,12 @@ if (location === 'post-101'){
     homeLink.style.display = 'none'
     siteBranding.style.display = "flex"
     siteBranding.style.flexDirection = "column"
-    siteBranding.style.maxWidth = "400px"
     siteBranding.innerHTML = "<h1 class='site-title'>Notre formation</h1><p class='presentation__header'>Notre formation a été prévu pour une durée de 8 mois avec 2 mois de stages compris. Celle-ci à une durée de 1260 heures.\n" +
         "Durant la formation nous avons vu différentes technologies du développement web, que vous retrouvez ci-dessous.</p>"
     siteHeader.style.background = "url('../wp-content/themes/nisarg-child/css/images/developpeur.jpg) " ;
     siteHeader.style.backgroundSize = "cover";
 } else {
     siteTitle.innerHTML = "Développeur Web & Web Mobile";
-    homeLink.style.maxWidth = "400px"
+    siteHeader.style.background = "url('./wp-content/themes/nisarg-child/css/images/group.webp')"
     siteHeader.classList.add("site__home-header")
-
 }
